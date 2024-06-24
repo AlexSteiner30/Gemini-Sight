a353a63e79316a98bc6cf3ac246ab17c8f503b32
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2024-06-24 19:06:20 +0200

    Updates

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = 8080;
 const allowedPages = ['index', 'admin', 'function', 'about'];
 
-
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, '/views')));
 
@@ -20,8 +19,6 @@ app.get('/:id', (req, res) => {
     if (allowedPages.includes(req.params.id)) res.render(req.params.id);
     else res.render("notFound");
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
