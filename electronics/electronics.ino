26692af935c25ace247217b43d4899a375f213f7
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2024-06-27 17:57:27 +0200

    WSS Gemini Payload

---
+++ electronics/electronics.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial (2,3);
+void  setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Enter AT Commands");
+  BTSerial.begin(9600);
+}
+
+void  loop() {
+  // put your main code here, to run repeatedly:
+  if(BTSerial.available()){
+    Serial.write(BTSerial.read());
+  }
+  if(Serial.available()){
+    BTSerial.write(Serial.read());
+  }
+}
