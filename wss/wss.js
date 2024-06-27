5a8751cb330a0be6aae5bf5dc277fb9dfe62cad6
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2024-06-27 23:58:01 +0200

    Media

--- wss/wss.js
+++ wss/wss.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const fs = require('fs');
 const { Database } = require('./func/db.js');
 const { Audio } = require('./func/audio.js');
 const helper = require('./func/helper.js');
@@ -20,21 +20,63 @@ wss.on('connection', function connection(ws) {
     try{
         if(data.toString('utf8').split('¬')[0] == 'speak'){
             const access_key = data.toString('utf8').split('¬')[1];
-            const text = data.toString('utf8').split('¬')[2];
-            const uuid = helper.uuidv4();
 
-            for (let i = 0; i < text.match(/.{1,150}/g).length; i++) {
-                //await audio.pcm(text.match(/.{1,150}/g)[i], access_key, i, uuid, ws);
-                ws.send('Audio created'); // FIND OTHER FREE API
+            if (db.find('access_key', access_key)) {
+                const text = data.toString('utf8').split('¬')[2];
+                const uuid = helper.uuidv4();
+
+                const response = (await ai.process_input(input));
+
+                try{
+                    for (let i = 0; i < text.match(/.{1,150}/g).length; i++) {
+                        //await audio.pcm(text.match(/.{1,150}/g)[i], access_key, i, uuid, ws);
+                        ws.send('Audio created'); // FIND OTHER FREE API
+                    }
+                }
+                catch{
+
+                    ws.send('Internal server error');
+                }
+            } else {
+                ws.send('Use a valid access key in order to access the API');
             }
         }
         else if(data.toString('utf8').split('¬')[0] == 'process'){
             const access_key = data.toString('utf8').split('¬')[1];
-            const text = data.toString('utf8').split('¬')[2];
 
-            const response = (await ai.process_data(text));
-            console.log(response);
-            ws.send('r' + response);
+            if (db.find('access_key', access_key)) {
+                try{             
+                    const text = data.toString('utf8').split('¬')[2];
+
+                    const response = (await ai.process_data(text));
+                    console.log(response);
+                    ws.send('r' + response);
+                }
+                catch{
+                    ws.send('Internal server error');
+                }
+            } else {
+                ws.send('Use a valid access key in order to access the API');
+            }
+        }
+        else if(data.toString('utf8').split('¬')[0] == 'media'){
+            const access_key = data.toString('utf8').split('¬')[1];
+
+            var base64Data = data.toString('utf8').split('¬')[2];
+            console.log('okay')
+            
+            fs.writeFileSync('out.png', Buffer.from(base64Data, 'base64'));
+
+            if (db.find('access_key', access_key)) {
+                try{             
+
+                }
+                catch{
+                    ws.send('Internal server error');
+                }
+            } else {
+                ws.send('Use a valid access key in order to access the API');
+            }
         }
         else{
             const access_key = data.toString('utf8').split('¬')[0];
@@ -67,7 +109,8 @@ wss.on('connection', function connection(ws) {
             }
         }
     }
-    catch{
+    catch(err){
+        console.log(err);
         ws.send('Wrong connection');
     }
   });
