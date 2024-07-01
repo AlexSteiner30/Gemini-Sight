6d0e70fa42e0ae1f9e5d3dd2998034fe3032ea1a
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-07-01 22:41:26 +0300

    Authentication

--- server/server.js
+++ server/server.js
@@ -5,71 +5,53 @@ const path = require("path");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bodyparser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
 const app = express();
 const PORT = 8080;
-const allowedPages = ['index', 'admin', 'function', 'about', 'signup', 'order'];
+const allowedPages = ['index', 'admin', 'function', 'about', 'order', 'notFound'];
 
 app.set("views", path.join(__dirname, "/views/ejs"));
 app.use(express.static(path.join(__dirname, '/views')));
 
 app.set('view engine', 'ejs');
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/:id', (req, res) => {
-    if (req.params.id == "admin" || req.params.id == "signup") res.render(req.params.id, {failed: false});
-    else if (allowedPages.includes(req.params.id)) res.render(req.params.id);
-    else res.render("notFound");
+    if (allowedPages.includes(req.params.id)) res.render(req.params.id, {
+        isLoggedIn: req.cookies["cookie-token"]
+    });
+    else res.redirect("notFound");
 });
 
 app.post('/signin', bodyparser.urlencoded(), async (req, res) => {
-    let name = req.body.username;
-    let password = req.body.password;
-    let found = false;
-    User.find({}).then(users => {
-        users.forEach(user => {
-            if (name == users.username && password == users.password) found = true;
+    let token = req.body.token;
+    
+    async function verify() {
+        const ticket = await client.verifyldToken({
+            idToken: token,
+            audience: process.env.CLIENT_ID,
         });
-        if (!found) res.render("admin", {failed: true});
-        else {
-            res.render("index");
-            //sign user in later
-        }
-    });
+        /* const payload = ticket .getPayload();
+        const userid = payload['sub']; */
+    }
+    verify().then(_ => {
+        res.cookie("cookie-token", token);
+        res.render("index");
+    }).catch(console.error);
 });
 
-app.post('/signup', bodyparser.urlencoded(), async (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let confirm = req.body.confirm;
-    if (password != confirm) res.render("signup", {failed: true});
-    let found = false;
-    User.find({}).then(users => {
-        users.forEach(user => {
-            if (username == users.username && password == users.password) found = true;
-        });
-        if (found) res.render("signup", {failed: true});
-        else {
-            let user = new User();
-            user.username = username;
-            user.password = password;
-            user.save().then(_ => {
-                res.render("index");
-            });
-        }
-    });
-});
-
-app.post("/logout", async (req, res) => {
-
-});
-
-app.post("/delete", async (req, res) => {
-
+app.get("/logout", async (req, res) => {
+    res.clearCookie('session-token');
+    res.redirect("index");
 });
 
 app.listen(PORT, _ => console.log(`Server running on port ${PORT}`));
