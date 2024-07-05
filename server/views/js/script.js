fce796a23e4217d6f3b82a792a72533b1d734137
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-07-05 21:00:14 +0300

    Remove some redundancy

--- server/views/js/script.js
+++ server/views/js/script.js
@@ -13,3 +13,23 @@ function signOut() {
         changeScreen("logout");
     });
 }
+
+function createNavAndFooter() {
+    let sign = (document.getElementById("logged-in").value ? '<a onClick="signOut()">Sign Out</a>' : '<a href="admin">Sign In</a>');
+    document.getElementsByTagName("body")[0].innerHTML = `
+        <nav>    
+            <div></div>
+            <a href="index">Home</a>
+            <a href="about">About Us</a>
+            <a href="function">Functioning</a>
+            <a href="order">Order</a>
+    `       +sign+`
+        </nav>
+    ` + document.getElementsByTagName("body")[0].innerHTML + `
+        <footer>
+            <p>Copyright (c) 2024 Alex Steiner</p>
+        </footer>
+    `;
+}
+
+createNavAndFooter();
