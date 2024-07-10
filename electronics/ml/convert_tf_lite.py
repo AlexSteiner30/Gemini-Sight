340e7776fd7ab9ca70371ce1cff09e85d1a19efa
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2024-07-10 16:47:23 +0200

    tf_lite_cpp

--- electronics/ml/convert_tf_lite.py
+++ electronics/ml/convert_tf_lite.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
-from tensorflow import keras
+import numpy as np
 
-model = keras.models.load_model('model.keras')
+model=tf.keras.models.load_model('model.keras')
+tflite_converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-
-with open('model.tflite', 'wb') as f:
-    f.write(tflite_model)
+tflite_model = tflite_converter.convert()
+open("tf_lite_model.tflite", "wb").write(tflite_model)
