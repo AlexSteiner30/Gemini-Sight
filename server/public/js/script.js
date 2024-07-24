dbe1d07cfe01957ecd2e5ab0eb245eb35ba7a5a4
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-07-24 23:59:25 +0300

    Better styling for chatbot

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -1,3 +1,5 @@
+const isChatOpen = "QWERTYUIOIHZR>UUTFGJVKJHKBKJBuoòfcvwouvbwòrifbwjkgrb hjkfvbwrfhjkby32rg239r23ur9023rh2v34igt84gf08gf4f83iub";
+
 String.prototype.replaceAt = function(start, end, str) {
     return this.substring(0, start) + str + this.substring(end);
 }
@@ -11,7 +13,7 @@ function createNavAndFooter() {
     if (document.getElementById("logged-in").value) {
         let chats = JSON.parse(document.getElementById("chats").value);
         let addition = "";
-        if (chats.length > 0 && chats != []) { 
+        if (chats.length > 0 && chats != []) {
             chats.forEach(x => {
                 if (x.role == "user") addition += "<div></div>";
                 addition += "<label>"+processOutput(x.parts[0].text)+"</label>";
@@ -20,13 +22,19 @@ function createNavAndFooter() {
         }
         document.getElementsByTagName("body")[0].innerHTML += `
             <form id='chat-area' method='POST' action='/chat'>
-                <input type="button" id="vis-btn" value="Chat">
                 <div id='chat-prev'><div id='chat-grid'>`+addition+`</div></div>
                 <input type='text' id='chat-input' placeholder='Enter prompt...' name='prompt'>
             </form>
+            
+            <button id='chat-btn'>
+                <img class='chat-img' src='../media/chat.png' alt='Open Chat'>
+                <img class='chat-img' src='../media/arrow.png' alt='Close Chat'>
+            </button>
         `;
-        document.getElementById("chat-prev").style.display = "none";
-        document.getElementById("chat-input").style.display = "none";
+        if (!window.localStorage.hasOwnProperty(isChatOpen)) localStorage[isChatOpen] = 'false';
+        let index = !(window.localStorage[isChatOpen] == 'true');
+        document.getElementsByClassName("chat-img")[index+0].style.display = "none";
+        document.getElementById("chat-area").style.opacity = (!index+0).toString();
 
         sign = '<a href="logout">Sign Out</a>';
     }
@@ -53,28 +61,45 @@ function createNavAndFooter() {
 createNavAndFooter();
 
 if (document.getElementById("logged-in").value) {
-    document.getElementById("vis-btn").addEventListener("click", function(e) {
-        if (e.target.value == "Close") {
-            document.getElementById("chat-area").style.animationDuration = "300ms";
-            document.getElementById("chat-area").style.animationFillMode = "forwards";
-            document.getElementById("chat-area").style.animationName = "hide";
+    let open = document.getElementsByClassName("chat-img")[0];
+    let close = document.getElementsByClassName("chat-img")[1];
+    let area = document.getElementById("chat-area");
+    document.getElementById("chat-btn").addEventListener("click", function(e) {
+        console.log(window.localStorage[isChatOpen]);
+        if (window.localStorage[isChatOpen] == "false") {
+            window.localStorage[isChatOpen] = 'true';
+            area.style.animationDuration = "300ms";
+            area.style.animationFillMode = "forwards";
+            area.style.animationName = "show";
 
+            open.style.animationDuration = "300ms";
+            open.style.animationFillMode = "forwards";
+            open.style.animationName = "hide";
             setTimeout(_ => {
-                document.getElementById("vis-btn").value = "Chat";
-                document.getElementById("chat-prev").style.display = "none";
-                document.getElementById("chat-input").style.display = "none";
-                document.getElementById("chat-area").style.animationName = "show";
+                open.style.display = "none";
+                close.style.opacity = "0";
+                close.style.display = "block";
+                close.style.animationDuration = "300ms";
+                close.style.animationFillMode = "forwards";
+                close.style.animationName = "show";
             }, 300);
         }
-        else if (e.target.value == "Chat") {
-            document.getElementById("chat-area").style.animationDuration = "300ms";
-            document.getElementById("chat-area").style.animationFillMode = "forwards";
-            document.getElementById("chat-area").style.animationName = "hide";
+        else {
+            window.localStorage[isChatOpen] = 'false';
+            area.style.animationDuration = "300ms";
+            area.style.animationFillMode = "forwards";
+            area.style.animationName = "hide";
+            
+            close.style.animationDuration = "300ms";
+            close.style.animationFillMode = "forwards";
+            close.style.animationName = "hide";
             setTimeout(_ => {
-                document.getElementById("vis-btn").value = "Close";
-                document.getElementById("chat-prev").style.display = "block";
-                document.getElementById("chat-input").style.display = "inline";
-                document.getElementById("chat-area").style.animationName = "show";
+                close.style.display = "none";
+                open.style.opacity = "0";
+                open.style.display = "block";
+                open.style.animationDuration = "300ms";
+                open.style.animationFillMode = "forwards";
+                open.style.animationName = "show";
             }, 300);
         }
     });
