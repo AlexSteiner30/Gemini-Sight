c8eeeb47ffc3d7b19f1ecd089826efb85daea73d
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2024-08-09 14:15:22 +0200

    Update wss.js

--- wss/wss.js
+++ wss/wss.js
@@ -3,7 +3,7 @@ const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < 1; i++) {
     cluster.fork();
   }
 }else{
