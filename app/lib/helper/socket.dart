3b6ced707e9cc2a8a769ca8b68940f22aa6c13bf
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2024-08-09 14:47:34 +0200

    test

--- app/lib/helper/socket.dart
+++ app/lib/helper/socket.dart
@@ -1,5 +1,5 @@
 import 'package:web_socket_client/web_socket_client.dart';
 
 final socket = WebSocket(
-  Uri.parse('ws://192.168.88.17:443'),
+  Uri.parse('ws://geminisight-3b0d77438896.herokuapp.com:4000'),
 );
